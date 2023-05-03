c760284df23ab0d464b740f953f16a420b42f200
JanHuysmans <12201074@student.pxl.be>
2023-05-03 09:06:06 +0200

    Oefeningen Deel 4 aangevuld + 4.2.1 -DOM

---
+++ Oefeningen_JS_Deel4/Oefeningen_deel4.2/oefening1.js
@@ -0,0 +1,13 @@
+let paragrafenRandomTekst = document.getElementsByTagName('randomtekst');
+let afbeeldingen = document.getElementsByTagName('img');
+let specialeTekst = document.getElementsByClassName('specieletekst');
+let vijfdeParagraaf = document.getElementsByTagName('p')[4];
+let a = document.getElementsByClassName("randomtekst")[2];
+
+
+
+console.log(paragrafenRandomTekst);
+console.log(afbeeldingen);
+console.log(specialeTekst);
+console.log(vijfdeParagraaf);
+console.log(a);
